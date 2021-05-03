3c14edcdb0705ff74b3a24b263560f37ed42cdd1
Ahsan <shakilahsan45@gmail.com>
2021-05-03 22:58:02 +0600

    Add PDFS in Read More btns

--- BSMRAU/_nuxt/slide.js
+++ BSMRAU/_nuxt/slide.js
@@ -1,38 +1,45 @@
-const datas = [{
+const datas = [
+    {
         slideImage: "1.jpg",
         heading: "Book Of Research Abstract 2018",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     {
         slideImage: "2.jpg",
         heading: "Book Of Research Abstract 2018-2019",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     {
         slideImage: "3.jpg",
         heading: "Book Of Research Abstract 2019",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     {
         slideImage: "2.jpg",
         heading: "Book Of Research Abstract 2018-2019",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     {
         slideImage: "1.jpg",
         heading: "Book Of Research Abstract 2018",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     {
         slideImage: "3.jpg",
         heading: "Book Of Research Abstract 2019",
         teacherImage: "Photo-Aminul-Sir-2-x-2-Edit-01-PP-150x150.jpg",
         teacherName: "Prof. Dr. A. K. M. Aminul Islam",
+        pdf: "",
     },
     // { slideImage: "", heading: "", teacherImage: "", teacherName: "" },
     // { slideImage: "", heading: "", teacherImage: "", teacherName: "" },
@@ -44,11 +51,11 @@ let query1 = window.matchMedia("(min-width:1741px)");
 let query2 = window.matchMedia("(max-width:1740px)");
 
 const slideCards = document.querySelector(".slide-cards");
-window.addEventListener("resize", displayMenuItems());
+window.addEventListener("DOMContentLoaded", displayMenuItems());
 
 function displayMenuItems() {
     let id = 0;
-    let displayMenu = datas.map(function(item) {
+    let displayMenu = datas.map(function (item) {
         return `    <div id='A${id++}' class="card-slide">
         <img class="card-image" src=${item.slideImage} alt="" />
         <div class="heading">
@@ -61,7 +68,7 @@ function displayMenuItems() {
         <hr />
         <div class="slide-footer">
             <div class="fotter-cover">
-                <a class="read-more" href=""> READ MORE </a>
+                <a class="read-more" href="${item.pdf}"> READ MORE </a>
                 <div class="span-arrow">
                     <span class="arrow"></span>
                     <span class="arrow"></span>
@@ -97,18 +104,16 @@ function displayMenuItems() {
 
     count = 0;
     size = card[0].clientWidth + 10;
+    let timeout = setInterval(change, 2000);
 
-    window.addEventListener("resize", function() {
-        slideCards.style.transition = "none";
+    window.addEventListener("resize", function () {
         size = card[0].clientWidth + 10;
     });
 
-    let timeout = setInterval(change, 2000);
-
-    slideCards.addEventListener("mouseover", function() {
+    slideCards.addEventListener("mouseover", function () {
         clearInterval(timeout);
     });
-    slideCards.addEventListener("mouseout", function() {
+    slideCards.addEventListener("mouseout", function () {
         timeout = setInterval(change, 2000);
     });
 
@@ -117,7 +122,7 @@ function displayMenuItems() {
     function change() {
         slideCards.style.transition = "transform 0.5s ease";
         count++;
-        slideCards.style.transform = "translateX(" + -size * count + "px";
+        slideCards.style.transform = "translateX(" + -size * count + "px)";
     }
 
     // TRANSITIONEND
@@ -126,7 +131,7 @@ function displayMenuItems() {
         if (allCards[count].id === "first_clone") {
             slideCards.style.transition = "none";
             count = card.length - count;
-            slideCards.style.transform = "translateX(" + -size * count + "px";
+            slideCards.style.transform = "translateX(" + -size * count + "px)";
         }
     });
 }
